7cbceff79620144ea2c568a066aeb7b7e4958ae5
Cesargarciajr <>
2023-11-16 02:28:41 +0000

    Add static folders and settings as well as procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn galetos.wsgi
--- galetos/settings.py
+++ galetos/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-cesargarciajr-galetos-or-p6wqnvb7tm.us2.codeanyapp.com',]
+    '8000-cesargarciajr-galetos-or-p6wqnvb7tm.us2.codeanyapp.com',
+    'localhost', 'galetos-orderapp-pp4.herokuapp.com',]
 
 
 # Application definition
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'galetos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
