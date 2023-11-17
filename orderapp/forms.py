80d682b44dc057c311f5b7a0e255aeda4a9f704a
Cesargarciajr <>
2023-11-17 02:47:32 +0000

    Installed django 2

--- orderapp/forms.py
+++ orderapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import DateInput
 from .models import NewOrderModel
 
 
--- orderapp/models.py
+++ orderapp/models.py
@@ -1,9 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Model for New Orders
+STATUS = ((0, "Pending"), (1, "Approved"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="order_post")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.title
 
 
 class NewOrderModel(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="new_order")
     # Costumer Details
     first_name = models.CharField(max_length=200, unique=False)
     last_name = models.CharField(max_length=200, unique=False)
@@ -15,6 +36,7 @@ class NewOrderModel(models.Model):
     date = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
         ordering = ['-created_on']
