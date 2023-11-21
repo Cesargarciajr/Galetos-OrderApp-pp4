297a09a9f1e3d350352720b6194dc7d409490f71
Cesargarciajr <>
2023-11-21 20:02:07 +0000

    set debug to false for deploy

--- galetos/settings.py
+++ galetos/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-cesargarciajr-galetos-or-p6wqnvb7tm.us2.codeanyapp.com',
