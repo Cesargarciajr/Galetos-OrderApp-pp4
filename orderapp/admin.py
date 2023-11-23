65a27eb29608c9a1f907923cb06f30915cb614fd
Cesargarciajr <>
2023-11-23 19:12:09 +0000

    Add status to order list and put to work send message in contact us page

--- orderapp/admin.py
+++ orderapp/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import NewOrderModel
+from .models import NewOrderModel, ContactFormModel
 
 
 @admin.register(NewOrderModel)
 class PostNewOrder(admin.ModelAdmin):
-    list_display = ('created_on', 'date', 'quantity')
+    list_display = ('created_on','first_name', 'date', 'quantity', 'status')
     list_filter = ('created_on', 'date')
     search_fields = ('created_on', 'date',
                      'first_name', 'last_name', 'phone_number', 'email')
@@ -12,3 +12,9 @@ class PostNewOrder(admin.ModelAdmin):
 
     def approve_orders(self, request, queryset):
         queryset.update(status=True)
+
+@admin.register(ContactFormModel)
+class PostNewMessage(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone_number', 'email')
+    list_filter = ('first_name', 'last_name', 'phone_number', 'email')
+    search_fields = ('first_name', 'last_name', 'phone_number', 'email')
