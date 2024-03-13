654dfe051d74adefcff0006a5198cf9e2a2df4de
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-03-13 23:33:20 +0000

    Added 404 custom page

--- galetos/urls.py
+++ galetos/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orderapp.urls')),
-]
+] 
+
+handler404 = 'galetos.views.handler404'
