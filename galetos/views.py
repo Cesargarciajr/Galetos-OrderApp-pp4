e5da8f549aa245cc263a2b85b9fe2777aea1d535
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-03-14 00:20:57 +0000

    testing custom 404 page

--- galetos/views.py
+++ galetos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
-    return render(request, "404.html", {})
+    return render(None, "404.html", status=404)
