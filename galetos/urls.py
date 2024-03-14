51d8ade593b0c3f56f68348d5326a4f19eabef8b
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-03-14 00:02:14 +0000

    changed views for 404 custom page

--- galetos/urls.py
+++ galetos/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import handler404
 
-handler404 = 'galetos.views.handler404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orderapp.urls')),
 ] 
+
+handler404 = 'galetos.views.handler404'
